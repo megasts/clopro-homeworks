7ce55fa20e25d8fb6b877990f2e11716f5b3e7f7
ShulgatiySV <megasts@yandex.ru>
2025-04-11 01:32:06 +0500

    v2

--- src/buket.tf
+++ src/buket.tf
@@ -8,20 +8,20 @@ resource "yandex_storage_bucket" "test" {
   bucket = local.bucket_name
   max_size   = 1073741000
   acl    = "public-read"
-  # server_side_encryption_configuration {
-  #   rule {
-  #     apply_server_side_encryption_by_default {
-  #       kms_master_key_id = yandex_kms_symmetric_key.key-a.id
-  #       sse_algorithm     = "aws:kms"
-  #     }
-  #   }
-  # }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = yandex_kms_symmetric_key.key-a.id
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
 
 resource "yandex_storage_object" "cute-cat-picture" {
   bucket = yandex_storage_bucket.test.bucket
   key    = var.object_name
-  # source = "./img/foto1.png"
-  source = "./img/foto2.jpg"
+  source = "./img/foto.jpg"
   acl = "public-read"
+  depends_on = [yandex_storage_bucket.test]
 }
